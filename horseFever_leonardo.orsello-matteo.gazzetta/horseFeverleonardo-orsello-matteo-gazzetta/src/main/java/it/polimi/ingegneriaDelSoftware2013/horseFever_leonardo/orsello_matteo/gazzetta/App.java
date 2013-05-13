74a4d027b2ed81c33b7fee8d7d815ae43894254d
killermuu <killermuu@86116526-42e8-43b4-9573-c51c1d0b40de>
2013-05-13 14:42:13 +0000

    first commit

---
+++ horseFever_leonardo.orsello-matteo.gazzetta/horseFeverleonardo-orsello-matteo-gazzetta/src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_leonardo/orsello_matteo/gazzetta/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingegneriaDelSoftware2013.horseFever_leonardo.orsello_matteo.gazzetta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
