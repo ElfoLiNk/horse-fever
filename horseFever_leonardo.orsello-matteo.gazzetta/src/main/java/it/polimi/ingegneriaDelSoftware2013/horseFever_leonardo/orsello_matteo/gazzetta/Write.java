1a3d56d9ca3d64c6e2e4a27b8767bff9317c339f
ElfoLiNk <ElfoLiNk@86116526-42e8-43b4-9573-c51c1d0b40de>
2013-05-22 17:04:48 +0000

    final write out ?

--- horseFever_leonardo.orsello-matteo.gazzetta/src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_leonardo/orsello_matteo/gazzetta/Write.java
+++ horseFever_leonardo.orsello-matteo.gazzetta/src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_leonardo/orsello_matteo/gazzetta/Write.java
@@ -3,38 +3,21 @@
  */
 package it.polimi.ingegneriaDelSoftware2013.horseFever_leonardo.orsello_matteo.gazzetta;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 
 /**
  * @author matteo
  * 
  */
-public class Write {
-	private static BufferedWriter bw;
-	private static FileOutputStream fos;
+final class Write {
 
 	public static void write(String s) {
-		fos = new FileOutputStream(java.io.FileDescriptor.out);
-		bw = new BufferedWriter(new OutputStreamWriter(fos));
-		try {
-			bw.write(s);
-		} catch (IOException e) {
-			System.out.println("Errore di flusso");
-		}
+		PrintStream out = System.out;
+		out.println(s);
 	}
 
 	public static void write(int i) {
-		fos = new FileOutputStream(java.io.FileDescriptor.out);
-		bw = new BufferedWriter(new OutputStreamWriter(fos));
-		try {
-			bw.write(i);
-			bw.close();
-			fos.close();
-		} catch (IOException e1) {
-			Write.write("Errore di flusso");
-		}
+		PrintStream out = System.out;
+		out.println(i);
 	}
 }
