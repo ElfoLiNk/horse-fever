f44f967fc5b82e4795006b711be2045ce4b23d8b
ElfoLiNk <ElfoLiNk@86116526-42e8-43b4-9573-c51c1d0b40de>
2013-05-27 12:55:07 +0000

    first fix scommessa

--- horseFever_leonardo.orsello-matteo.gazzetta/src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_leonardo/orsello_matteo/gazzetta/Main.java
+++ horseFever_leonardo.orsello-matteo.gazzetta/src/main/java/it/polimi/ingegneriaDelSoftware2013/horseFever_leonardo/orsello_matteo/gazzetta/Main.java
@@ -21,25 +21,18 @@ public class Main {
 		public static void main(String[] args) {
 			Partita partita = new Partita();
 			partita.setListe();
-			Write.write(partita.getListaazioni().toString());
-			List<CarteAzione> lista = CarteAzione.checkLetteraCarteAzione(partita.getListaazioni());
-			
-			if(!lista.isEmpty())Write.write(lista.toString());
-			/*partita.setScuderie();
+			partita.setScuderie();
 			partita.setNumGiocatori();
 			partita.setGiocatori();
 			partita.setCarteAzione();
 			partita.setCarteAzione();
-			//Scuderia temp = new Scuderia();
-			//2temp.effettuascommessa();
+			partita.randomPrimogiocatore();
+			partita.scommessa();
 			partita.truccoCorsa();
-			partita.movimento(2);*/
-			//CarteMovimento Prima = new CarteMovimento();
-			//Prima.setMovimento();
-			
-			//Write.write(Prima.getMovimento(5));
-
-			// TODO Auto-generated method stubjjj
+			partita.scommessa();
+			partita.corsa();
+			//partita.pagascomessa();
+			//partita.aggiornaquotazioni();			
 
 		}
 
